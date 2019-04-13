594e2fa58f0e91da925e10add6ee94645c543a5c
Rayna McGinnis <raynamcginnis@gmail.com>
2019-04-13 10:02:55 -0600

    Updates

---
+++ precache-manifest.d7cb8dda6370918cbf41d5a4704d1cb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "d888022b64cea7621d45",
+    "url": "/Clicky-Game/static/js/main.49b2cd08.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "d888022b64cea7621d45",
+    "url": "/Clicky-Game/static/css/main.aa7f3548.chunk.css"
+  },
+  {
+    "revision": "7d4e8856f7116fd3af53a0c6a119c6e9",
+    "url": "/Clicky-Game/index.html"
+  }
+];
