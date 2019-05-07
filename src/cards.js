2cb5ad2156951418ac1b2d8a33220a4d25a70190
Rayna McGinnis <raynamcginnis@gmail.com>
2019-05-06 21:29:23 -0600

    images

--- src/cards.js
+++ src/cards.js
@@ -1,37 +1,36 @@
-
 export const cats = [{
-    "id": 1,
-     "image": './public/images/catphoto1.jpg'
-  },
-  {
-    "id": 2,
-    "image": "catphoto2.jpg"
-  },
-  {
-    "id": 3,
-    "image": "catphoto3.jpg"
-  },
-  {
-    "id": 4,
-    "image": "catphoto4.jpg"
-  },
-  {
-    "id": 5,
-    "image": "catphoto5.jpg"
-  },
-  {
-    "id": 6,
-    "image": "catphoto6.jpg"
+  "id": 1,
+  "image": 'catphoto1.jpg'
+},
+{
+  "id": 2,
+  "image": "catphoto2.jpg"
+},
+{
+  "id": 3,
+  "image": "catphoto3.jpg"
+},
+{
+  "id": 4,
+  "image": "catphoto4.jpg"
+},
+{
+  "id": 5,
+  "image": "catphoto5.jpg"
+},
+{
+  "id": 6,
+  "image": "catphoto6.jpg"
 
-  },
-  {
-    "id": 7,
-    "image": "catphoto7.jpg"
+},
+{
+  "id": 7,
+  "image": "catphoto7.jpg"
 
-  },
-  {
-    "id": 8,
-    "image": "catphoto8.jpg"
+},
+{
+  "id": 8,
+  "image": "catphoto8.jpg"
 
-  }
+}
 ]
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./Card.css";
 
@@ -6,11 +5,7 @@ import "./Card.css";
 
 const Card = props => (
   <div className="card img-container hover">
-<<<<<<< HEAD
-    <img alt={props.name} src={`${props.image}`} id={props.id}
-=======
     <img alt={props.name} src={`../../images/${props.image}`} id={props.id}
->>>>>>> 637300c987b8572dcf83332172b05e2033899251
       onClick={() => props.shuffleScoreCard(props.id)} className='shuffleScore' />
   </div>
 );
